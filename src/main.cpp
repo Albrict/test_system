3959598b0d045a64aa91a50af7f3cb6da1ddae72
Albrict <karelinaleksj@gmail.com>
2023-06-01 22:55:05 +0200

    Added basic database support

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,40 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Double_Window.H>
 
-#include "registration_state.hpp"
+#include "registration_group.hpp"
 #include "group_manager.hpp"
 
 namespace {
 
     Fl_Double_Window *win = nullptr;
+    PGconn *connection = nullptr;
+    
+    void main_window_callback(Fl_Widget *widget, void* data)
+    {
+        Fl_Double_Window *main_window = (Fl_Double_Window*)widget;
+        main_window->hide();
+        PQfinish(connection);
+    }
+
+    void die()
+    {
+        PQfinish(connection);
+        exit(EXIT_FAILURE);
+    }
 
     void init()
     {
+        // Connect to database
+        connection = PQconnectdb("host=localhost port=5432 dbname=test_db user=postgres");
+        
+        if (PQstatus(connection) != CONNECTION_OK) {
+            Fl::fatal(PQerrorMessage(connection));
+            die();
+        }
+        // Init GUI
         win = new Fl_Double_Window(1024, 768, "Testing System");
-        GroupManager::getInstance().newGroup(*new RegistrationState);
+        win->callback(main_window_callback);
+        GroupManager::getInstance().newGroup(*new RegistrationGrop(*connection));
         win->fullscreen();
         win->show();
     }
---
+++ src/registration_group.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Group.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Choice.H>
+
+#include <libpq-fe.h>
+#include <vector>
+
+class RegistrationGrop: public Fl_Group {
+    std::vector<Fl_Input*> input_field_array;
+    
+    Fl_Button *button;
+    Fl_Choice *combobox;
+
+    PGconn &connection;
+public:
+    RegistrationGrop(PGconn &p_connection);
+private:
+    static void say_callback(Fl_Widget *widget, void *data);
+};
